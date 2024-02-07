75299c2d4b8f91ce609041e0c53f1a9489912fea
Zoliswa-a . <bamzoliswaa@gmail.com>
2024-02-07 14:52:58 +0000

    validating data

--- run.py
+++ run.py
@@ -12,8 +12,52 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('live_love_yoga')
 
-arrived = SHEET.worksheet('arrived')
+def get_arrived_data():
+    """
+    Getting arrived figures from the user from the last yoga classes
+    """
+    while True:
+        print("Please enter arrived data from the last yoga class")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 1,2,3,4,5,6\n")
+    
+        data_str = input("Enter your data here: ")
 
-data = arrived.get_all_values()
+        arrived_data = data_str.split(",")
 
-print(data)
+        if validate_data(arrived_data):
+            print("Data Approved")
+            break
+
+    return arrived_data
+
+
+def validate_data(values):
+    """
+    We're validating 
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
+
+def update_arrived_worksheet(data):
+    """
+    Update arrived worksheet
+    """
+    print("updating arrived worksheet...\n")
+    arrived_worksheet = SHEET.worksheet("arrived")
+    arrived_worksheet.append_row(data)
+    print("Arrived worksheet successfully updated.\n")
+
+
+data = get_arrived_data()
+arrived_data = [int(num) for num in data]
+update_arrived_worksheet(arrived_data)
